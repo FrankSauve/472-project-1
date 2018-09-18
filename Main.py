1440b0d49a82ce22365aaa6674f78f87091458c2
LeCleric <13441191+LeCleric@users.noreply.github.com>
2018-09-18 12:56:11 -0400

    Modified Main.py to have input_puzzle contain the input shown in the assignment. Commented out the randomizer. Prettied up the temporary console output

--- Main.py
+++ Main.py
@@ -4,21 +4,22 @@ from Puzzle import Puzzle
 
 # Asks the user for the initial puzzle TODO: Commented to make testing faster
 # input_puzzle = input("Enter the board separated by commas: ").replace(" ", "").split(",")
-input_puzzle = "0,1,2,3,4,5,6,7,8,9,10,11".split(",")  # TODO: Remove for release
+input_puzzle = "1,0,3,7,5,2,6,4,9,10,11,8".split(",")  # TODO: Remove for release
 
 # Converts to int
 input_puzzle = list(map(int, input_puzzle))
 
 # Shuffles the puzzle TODO: Remove for release
-random.shuffle(input_puzzle)
+# random.shuffle(input_puzzle)
 
 # Check if the input_puzzle is of length 12
 if len(input_puzzle) != 12:
     raise Exception("Puzzle must have 12 tiles")
 
+print('Initial puzzle state:')
 puzzle = Puzzle()
 puzzle.set_puzzle(input_puzzle)
 puzzle.print()
 
-print(puzzle.get_possible_moves())
-print(puzzle.get_tile_letter(1, 1))
+print('\nPossible moves: \n' + str(puzzle.get_possible_moves()))
+# print(puzzle.get_tile_letter(1, 1))
